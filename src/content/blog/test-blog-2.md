3b7da2c95879718d5053fddbdbbcac3283f180a1
Aseem Ratha <aseemratha@gmail.com>
2023-11-22 00:19:50 -0500

    changing around the files and config, added author

--- src/content/blog/test-blog-2.md
+++ src/content/blog/test-blog-2.md
@@ -2,6 +2,7 @@
 title: test blog 2
 description: this is test 2
 pubDate: 2023-11-22T05:02:10.850Z
-heroimage: /assets/vic-records-logo-2022.png
+author: Aseem Ratha
+heroimage: /vic-records-logo-2022.png
 ---
 this is the body of the test blog.
--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const blog = defineCollection({
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+		author: z.string().optional(),
 		heroImage: z.string().optional(),
 	}),
 });
