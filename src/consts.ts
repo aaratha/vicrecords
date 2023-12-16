01ee24b33f2a8476122d681ac29faa1c9dd0bd04
Aseem Ratha <aseemratha@gmail.com>
2023-12-15 19:08:12 -0500

    added space between vic and records

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'VicRecords';
+export const SITE_TITLE = 'Vic Records';
 export const SITE_DESCRIPTION = 'Welcome to VicRecords';
